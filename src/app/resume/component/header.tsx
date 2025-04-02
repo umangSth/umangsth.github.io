4f919c443ea735be4cf51acf6fac37840f4c4d61
umanga <Umang.sth099@gmail.com>
2025-04-02 16:08:46 -0400

    style adjustment for the header-nav

--- src/app/resume/component/header.tsx
+++ src/app/resume/component/header.tsx
@@ -39,9 +39,9 @@ export const Header = (props: React.PropsWithChildren) => {
 
     return (
         <>
-        <header className="h-24 sm:h-32 flex items-center z-30 w-full fixed ">
-            <div className="container mx-auto px-6 flex items-center justify-end">
-                <div className="flex items-center gap-4 sm:gap-6" onMouseLeave={() => toggleNavState('header-case')}>
+        <header className="h-24 sm:h-32 flex items-center z-30 w-full fixed  ">
+            <div className="container mx-auto px-1 sm:px-6 flex items-center  justify-end">
+                <div className="flex items-center gap-0 sm:gap-6" onMouseLeave={() => toggleNavState('header-case')}>
                     <nav className={`
                       font-sen text-gray-800 dark:text-white uppercase text-lg items-center 
                           transition-all duration-1000 ease-in-out
@@ -51,8 +51,10 @@ export const Header = (props: React.PropsWithChildren) => {
                           relative lg:static`}
                     >
                         <div className="flex  items-center gap-4">
-                            <Link href="/" className="shadow-lg shadow-gray-300/50 hover:shadow-xl flex items-center px-4 py-2 bg-white dark:bg-gray-700 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 transition-all group">
-                                <svg id="resume" viewBox="0 0 24 24" className="h-5 w-5 mr-2">
+                            <Link href="/" className="shadow-lg shadow-gray-300/50 hover:shadow-xl flex items-center bg-white dark:bg-gray-700 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 transition-all group
+                                px-2 py-1 lg:px-4 lg:py-2
+                            ">
+                                <svg id="resume" viewBox="0 0 24 24" className="h-4 w-4 mr-2 lg:w-8 lg:h-6 ">
                                     {/* Main document body */}
                                     <rect x="2" y="4" width="18" height="20" rx="2" className="" />
 
@@ -68,25 +70,33 @@ export const Header = (props: React.PropsWithChildren) => {
                                     <circle cx="6" cy="17" r="1" className="fill-white" />
                                     <rect x="8" y="16.5" width="8" height="1" className="fill-blue-300" />
                                 </svg>
-                                <span className="text-gray-700 dark:text-gray-200 ">Resume</span>
+                                <span className="text-gray-700 dark:text-gray-200  text-xs  lg:text-lg">Resume</span>
                             </Link>
-                            <Link href="/drawings" className="shadow-lg shadow-gray-300/50 hover:shadow-xl flex items-center px-4 py-2 bg-white dark:bg-gray-700 rounded-lg hover:bg-gray-100 drak:hover:bg-gray-600 transition-all group">
-                                <svg id="pencil" viewBox="0 0 24 24" className="h-5 w-5 mr-0">
+                            <Link href="/drawings" className="shadow-lg shadow-gray-300/50 hover:shadow-xl flex items-center bg-white dark:bg-gray-700 rounded-lg hover:bg-gray-100 drak:hover:bg-gray-600 transition-all group
+                                 px-2 py-1 lg:px-4 lg:py-2
+                            ">
+                                <svg id="pencil" viewBox="0 0 24 24" className="h-4 w-4 mr-0  lg:w-8 lg:h-6 ">
                                     <polygon points="4,20 7,20 5,25 6,25" className="" />
                                     <polygon points="1,14 9,14 7,20 4,20" className=" fill-orange-400" />
                                     <rect x="0.9" y="0" width="8.5" height="15" rx="1" ry="2" className="" />
                                 </svg>
-                                <span className="text-gray-700 dark:text-gray-200 ">Drawings</span></Link>
-                            <Link href="/games" className="shadow-lg shadow-gray-300/50 hover:shadow-xl flex items-center px-4 py-2 bg-white dark:bg-gray-700 rounded-lg hover:bg-gray-100 drak:hover:bg-gray-600 transition-all group">
-                                <svg id="pacman" viewBox="0 0 24 24" className="h-5 w-5 mr-2">
+                                <span className="text-gray-700 dark:text-gray-200 text-xs lg:text-lg">Drawings</span></Link>
+                            <Link href="/games" className="shadow-lg shadow-gray-300/50 hover:shadow-xl flex items-center bg-white dark:bg-gray-700 rounded-lg hover:bg-gray-100 drak:hover:bg-gray-600 transition-all group
+                                px-2 py-1 lg:px-4 lg:py-2
+                            ">
+                                <svg id="pacman" viewBox="0 0 24 24" className="h-4 w-4 mr-2 lg:w-8 lg:h-6 ">
                                     <circle cx="50%" cy="50%" r="25%" />
                                 </svg>
-                                <span className="text-gray-700 dark:text-gray-200 ">Games</span>
+                                <span className="text-gray-700 dark:text-gray-200 text-xs lg:text-lg">Games</span>
                             </Link>
                         </div>
                     </nav>
+                
                     <button
-                        className={`flex flex-col ml-4 transition-opacity duration-600 bg-white py-3 px-3 rounded-full hover:bg-gray-200 
+                         id="hamIcon"
+                        className={`flex flex-col  transition-opacity duration-600 bg-white 
+                            py-1.5 px-1 lg:py-2 lg:px-2  
+                            rounded-full hover:bg-gray-200 
                           ${isScrolled ? 'lg:flex pointer' : ''}
                           ${isScrolled && !isNavOpen ? 'opacity-100' : 'opacity-0 lg:opacity-0'}
                           shadow-lg shadow-gray-400/50 hover:shadow-xl
@@ -95,11 +105,11 @@ export const Header = (props: React.PropsWithChildren) => {
                         onMouseLeave={() => { setIsNavOpen(false); }}
                         onClick={() => setIsNavOpen(!isNavOpen)}
                     >
-                        <span className={`w-6 h-1 bg-gray-800 dark:bg-white mb-1 
+                        <span className={`w-4 h-0.5  lg:w-6 lg:h-1 rounded-full bg-gray-800 dark:bg-white mb-0.5 lg:mb-1 
                           `} />
-                        <span className={`w-6 h-1 bg-gray-800 dark:bg-white mb-1 
+                        <span className={`w-4 h-0.5 lg:w-6 lg:h-1 rounded-full bg-gray-800 dark:bg-white mb-0.5 lg:mb-1 
                           `} />
-                        <span className={`w-6 h-1 bg-gray-800 dark:bg-white mb-1 
+                        <span className={`w-4 h-0.5 lg:w-6 lg:h-1 rounded-full bg-gray-800 dark:bg-white mb-0.5 lg:mb-1 
                             
                           `} />
 
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -7,11 +7,15 @@ import { TimeLine } from "./component/time-line";
 export default function Resume() {
     return (
         <>
-          <div id="main" className="min-h-screen py-15  gap-16 font-[family-name:var(--font-geist-sans)]
-                                                    2xl:px-60 2xl:pb-70 2xl:pt-40
-                                                    xl:py-40 xl:px-40
-                                                    sm:py-15 sm:px-35
-                                                    lg:px-20 lg:py-32">
+          <div
+                id="main"
+                className="min-h-screen py-15 gap-16 font-[family-name:var(--font-geist-sans)]
+                  2xl:px-60 2xl:pt-40 2xl:pb-20 // Adjusted 2xl bottom padding
+                  xl:py-40 xl:px-40 xl:pb-20 // Adjusted xl bottom padding
+                  sm:py-15 sm:px-35 sm:pb-10 // Adjusted sm bottom padding
+                  lg:px-20 lg:py-32 lg:pb-15 // Adjusted lg bottom padding
+                "
+  >
             <About />
             <TimeLine />
             <Projects />
