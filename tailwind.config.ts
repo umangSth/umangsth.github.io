731bdc7454292ed193478d559168c7749a5da5db
umanga <Umang.sth099@gmail.com>
2025-04-14 12:37:18 -0400

    tailwind import issues ! text fix 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,22 @@
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
-};
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#ffffff',
+        'secondary1': '#c8e6b9d7',
+        'secondary2': '#c8e6b9d7',
+        'secondary3': '#c8e6b9d7',
+        'secondary4': '#c8e6b9d7',
+        'secondary5': '#c8e6b9d7',
+        'secondary6': '#c8e6b9d7',
+      },
+    },
+  },
+  plugins: [],
+
+} satisfies Config
