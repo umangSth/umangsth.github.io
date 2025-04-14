3d7c9c24892b2bd802663fb4d251938f9dc031da
umanga <Umang.sth099@gmail.com>
2025-04-14 12:17:10 -0400

    tailwind import issue, ! test fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {unoptimized: true},
+
   /* config options here */
   // basePath: '/my-resume',
 };
